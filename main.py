94136af290a4fc4d799fb7b66dd027bd259ca83c
RaidTheWeb <therealraidtheweb@gmail.com>
2020-04-09 00:03:24 +0000

    modified code

--- main.py
+++ main.py
@@ -2,27 +2,29 @@ import redbot
 import praw
 import datetime
 import subprocess
+import os
+import time
 
 class BonikkuCore:
-  def monitor():
-    reddit = praw.Reddit('bonikku')
-    subreddit = reddit.subreddit('fl80')
-    for submission in subreddit.stream.submissions():
-      command = submission.title
-      if submission.author == 'RaidTheWeb':
+  def monitor(monitored):
+    while True:
+      reddit = praw.Reddit('bonikku')
+      subreddit = reddit.subreddit(monitored)
+      for submission in subreddit.stream.submissions():
+        command = submission.title
         print('Recieved Command:\n')
         print('Command: ' + submission.title)
         print('Arguments: ' + submission.selftext)
         print('Author: ' + str(submission.author))
         print('ID: ' + submission.id)
         print('---------------------------------\n')
-        if submission.title.lower() == 'time':
+        if submission.title.lower() == 'bonikku:time':
           response = str(datetime.datetime.now())
 
-        elif submission.title.lower() == 'useragent':
+        elif submission.title.lower() == 'bonikku:useragent':
           response = 'Bonikku AI EN_NZ 1.0rc1'
 
-        elif submission.title.lower() == 'sendmail':
+        elif submission.title.lower() == 'bonikku:sendmail':
           args = submission.selftext
           argslist = args.split(',')
           if len(argslist) == 3:
@@ -32,32 +34,36 @@ class BonikkuCore:
             redbot.sendmail(redditor, title, message)
             response = 'Sent {} to {}'.format(title, redditor)
           else:
-            response = 'INFO: [bonikku]: not enough arguments.'
-        elif submission.title.lower() == 'bash':
-          response = subprocess.getoutput(submission.selftext)
+            response = 'INFO: [bonikku]:  not enough arguments.'
+        elif submission.title.lower() == 'bonikku:bash':
+            response = subprocess.getoutput(submission.selftext)
+        elif submission.title.lower() == 'bonikku:python':
+          temp = open('tmp.py', 'w+')
+          f = submission.selftext
+          temp.write(f)
+          temp.close()
+          cmd = subprocess.run(["python", "tmp.py"], capture_output=True)
+          response = cmd.stdout.decode()
+        elif submission.title.lower() == 'bonikku:monitor':
+          args = submission.selftext
+          monitored = args
+          response = 'Changed monitor target subreddit to r/{}.'.format(monitored)
 
         else:
-          response = 'INFO: [bonikku]: unknown function.'
+          if submission.title.lower().startswith('bonikku:'):
+            response = 'INFO: [bonikku]: unknown function.'
+          else:
+            continue
 
-
-        print('Responding...\n')
-        responder = reddit.submission(id=submission.id)
-        responder.reply(response)
-        print('Response: ' + response)
-
-
-      else:
-        print('WARNING: [bonikku]: unauthorized redditor active on r/fl80\n' + str(submission.author) + ' posted:\n')
-        print('Recieved Command:\n')
-        print('Command: ' + submission.title)
-        print('Arguments: ' + submission.selftext)
-        print('Author: ' + str(submission.author))
-        print('ID: ' + submission.id)
-        print('---------------------------------\n')
-        print('Reporting...\n')
-        redbot.sendmail('RaidTheWeb', 'WARNING: [bonikku]: unauthorized redditor active on r/fl80', 'At {} {} attempted to send a command to Bonikku. Take immediate action!'.format(datetime.datetime.now(),str(submission.author)))
-        print('Infiltration reported to RaidTheWeb ( MODERATOR )')
+        try:
+          print('Responding...\n')
+          responder = reddit.submission(id=submission.id)
+          responder.reply(response)
+          print('Response: ' + response)
+          break
+        except:
+          pass
 
 
 bonikku = BonikkuCore
-bonikku.monitor()
+bonikku.monitor('fl80')
